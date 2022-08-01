e57598ea79f494450ad650698f2caa88458d4fc8
phanisaiadabala <82699078+phanisaiadabala@users.noreply.github.com>
2022-08-01 15:46:10 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,20 @@
+def newGit(repo)
+{
+  git "${repo}"
+}
+
+def newMaven()
+{
+   sh 'mvn package' 
+}
+
+def newDeploy(jobname,ip,context)
+{
+   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def runSelenium(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
+
